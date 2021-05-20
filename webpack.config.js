0a94a7964aea067ef6de3a5ec98a7eaa87dcb668
Your Name <you@example.com>
2021-05-20 11:47:22 +0300

    бьютифизация выходного html

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,48 @@
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
+import * as fs from "fs";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+const PAGES_DIR = `./src/pug/pages/`
+const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'))
+
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import HtmlWebpackPugPlugin from 'html-webpack-pug-plugin';
+// import CleanWebpackPlugin from 'clean-webpack-plugin';
 
 export default {
+
+    module: {
+
+        rules: [
+            {
+                test: /\.pug$/,
+                exclude: /(node_modules)/,
+                use: [
+                    'pug-loader?pretty=true',
+                    // {loader: ''}
+                ]
+            }
+        ]
+    },
+
     entry: './index.js',
-    mode: 'production',
+    mode: 'development',
     output: {
         path: __dirname + '/dist',
         filename: 'index_bundle.js'
     },
     plugins: [
         new HtmlWebpackPlugin({
-            filename: 'output.pug',
-            minify: false
+            template: `${PAGES_DIR}/index.pug`,
+            filename: 'index.html',
+            minify: false,
+            inject: true
         }),
-        new HtmlWebpackPugPlugin()
+        // new HtmlWebpackPugPlugin(),
+        // new CleanWebpackPlugin(['dist']),
     ]
 }
